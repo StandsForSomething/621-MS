77b6128b6a5d1e1275e8055c3283417a41bc10e5
unknown <chewchee8@gmail.com>
2018-02-09 15:01:52 -0500

    get libraries set up

--- include/main.h
+++ include/main.h
@@ -26,6 +26,10 @@
 #define MAIN_H_
 
 #include <API.h>
+#include "buttons.h"
+#include "fbc.h"
+#include "fbc_pid.h"
+#include "mtrmgr.h"
 
 // Allow usage of this file in C++ programs
 #ifdef __cplusplus
